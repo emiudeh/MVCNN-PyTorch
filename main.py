525b0bfda1f02db8c8dd15311e512ade9e8ab7a2
emiudeh <udehemeka.edu@gmail.com>
2019-09-10 15:15:24 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from controller_our_case import train_MVCNN
+from test_our_case import test_MVCNN
+
+case_description = "6_instances"
+# train_MVCNN(case_description)
+
+
+test_description = "insert some description of test case."
+test_MVCNN(test_description)
